dc5ab76beb1f0b8621d7e34718cd2f0a2b621023
j.savicheva <j.savicheva@innopolis.ru>
2020-03-20 20:49:57 +0300

    init project

---
+++ src/main/java/Application.java
@@ -0,0 +1,12 @@
+/**
+ * Created on 20.03.2020.
+ *
+ * @authot Julia Savicheva
+ */
+public class Application {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/main/java/Pattern.java
@@ -0,0 +1,7 @@
+/**
+ * Created on 20.03.2020.
+ *
+ * @authot Julia Savicheva
+ */
+public class Pattern {
+}
